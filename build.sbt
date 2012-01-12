2f2803b284100e0d4b4f0c1ce5f2b88876a84383
Gregg <gregg@wordnik.com>
2012-01-11 17:38:01 -0800

    initial commit

---
+++ build.sbt
@@ -0,0 +1,12 @@
+import AssemblyKeys._
+
+name := "ivy2sbt"
+
+version := "1.0"
+
+scalaVersion := "2.9.1"
+
+mainClass := Some("Ivy2Sbt")
+
+seq(assemblySettings: _*)
+
---
+++ src/main/scala/Ivy2Sbt.scala
@@ -0,0 +1,17 @@
+import xml._
+
+object Ivy2Sbt {
+  def main(args: Array[String]) {
+    if (args.size != 1) println("usage: Ivy2Sbt <ivy-file-name>")
+    val ivy = XML.loadFile(args(0))
+    val deps = ivy \\ "dependency"
+    println("libraryDependencies ++= Seq(")
+    deps.foreach(outputSbtDepLine)
+    println(")")
+  }
+
+  def outputSbtDepLine(dep: Node) = {
+    println("  \"%s\" %s \"%s\" %s \"%s\",".format(dep.attribute("org").getOrElse(""), "%", dep.attribute("name").getOrElse(""), "%", dep.attribute("rev").getOrElse("")))
+  }
+
+}
